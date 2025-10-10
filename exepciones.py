78b67b5eb429e351c45c2ea554b34e91ab206420
DarioAndreatiniUADE <dandreatini@uade.edu.ar>
2025-10-09 23:29:30 -0300

    Clase de Excepsiones

---
+++ exepciones.py
@@ -0,0 +1,22 @@
+# Excepsiones - son los mensajes que tira el IDE que ocurren durante el tiempo de ejecucion
+
+# Index Error - cuando accedo a una secuencia con un indice que no existe.
+
+# Error de Sintaxis - Error en la escritura
+
+# Errores de semantica - No causan errores durante la ejecucion pero devuelven algo que no es lo buscado
+
+# "Mira antes de saltar" - Evita caer en el error
+
+# "Es mas facil pedir perdon que permiso" - es en el uso de Try y Except
+
+
+# Opcional - Finally (Limpia)
+
+# Opcional - Else 
+
+# Opcional - as e (Sirve para simplificar todos los casos de excepsion)
+
+
+
+
