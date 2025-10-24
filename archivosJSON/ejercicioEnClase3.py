75fe2b9dc740664a7d9a001b1168ff368b7734fc
DarioAndreatiniUADE <dandreatini@uade.edu.ar>
2025-10-23 21:15:26 -0300

    Clase de archivosJSON

---
+++ archivosJSON/ejercicioEnClase3.py
@@ -0,0 +1,20 @@
+"""
+La escuela organiza un Concurso de Talentos y quiere registrar a los participantes en un archivo talentos.json.
+ 
+Cada participante tiene:
+- Nombre
+- Edad
+- Puntaje (del 1 al 10)
+ 
+El programa debe:
+- Pedir los datos de varios participantes (hasta que el usuario escriba “fin”).
+- Guardar todos los datos en un archivo JSON llamado talentos.json.
+- Leer el archivo y mostrar:
+- El promedio de puntaje del grupo.
+- El participante con mayor puntaje
+- Todos los participantes con puntaje mayor o igual a 8.
+
+"""
+
+def ingreseParticipante():
+    pass
